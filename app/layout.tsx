eed22ebb3688c255089c1b36899aae00f7fe14bd
“Adi91108” <emailaddr>
2025-03-25 15:43:52 +0530

    Auth UI completed Now Homepage begin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -14,7 +15,7 @@ const monaSans = Mona_Sans({
 
 export const metadata: Metadata = {
   title: "PrepPeer",
-  description: "An AI-powered interview platform.",
+  description: "An AI-powered platform for preparing for mock interview",
 };
 
 export default function RootLayout({
@@ -24,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${monaSans.className}  antialiased`}>{children}</body>
+      <body className={`${monaSans.className}  antialiased pattern`}>
+        {children}
+
+        <Toaster />
+      </body>
     </html>
-  );   
+  );
 }
