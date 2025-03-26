820ac3e58677908d37c2a9aafd0b760418274bab
“Adi91108” <emailaddr>
2025-03-26 13:18:42 +0530

    Auth implementation done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,12 +5,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import FormField from "./FormField";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -35,16 +41,46 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        console.log("Sign Up", values);
+        const { name, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+        // console.log("Sign Up", values);
         toast.success("Account created Successfully.");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign In failed");
+          return;
+        }
+
+        await signIn({ email, idToken });
         toast.success("Sign in Successfully.");
         router.push("/");
-        console.log("Sign In", values);
+        // console.log("Sign In", values);
       }
     } catch (error) {
       console.log(error);
@@ -90,7 +126,6 @@ const AuthForm = ({ type }: { type: FormType }) => {
               placeholder="Your Password"
               type="password"
             />
-           
 
             <Button className="btn" type="submit">
               {isSignIn ? "Sign in" : "Create an Account"}
---
+++ firebase/client.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnhRcA8AF98vGNRj9RISRmcIbOPGiUt-U",
+  authDomain: "preppeer-2d1a9.firebaseapp.com",
+  projectId: "preppeer-2d1a9",
+  storageBucket: "preppeer-2d1a9.firebasestorage.app",
+  messagingSenderId: "1037112059314",
+  appId: "1:1037112059314:web:121f73e47c76516f7c70ab",
+  measurementId: "G-YRFPFLTNM7",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
